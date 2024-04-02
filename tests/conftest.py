058e8cbd15d9679d418c66f7d0cffba174a26732
Björn Boschman <bjoern.boschman@innovid.com>
2024-04-02 14:04:25 -0300

    Fixed DeprecationWarning

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 from app.main import app
 
 @pytest.fixture
@@ -8,5 +8,6 @@ def anyio_backend():
 
 @pytest.fixture
 async def client():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
         yield ac
